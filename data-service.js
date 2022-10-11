ec59394e7917612daaeb705d33d9d633c09b9477
ChriSPMinis <isung.inc@gmail.com>
2022-10-10 20:17:04 -0400

    Updated for A3
    Updated for A3

--- data-service.js
+++ data-service.js
@@ -6,30 +6,116 @@ module.exports.initialize = function() {
   return new Promise((resolve, reject) => {
     fs.readFile('./data/students.json', 'utf8', (err, studentData) => {
       if (err) {
-        return reject(new Error('unable to read file.'));
+        reject(new Error('unable to read file.')); return;
       }
-      fs.readFile('./data/programs.json', 'utf8', (err, programData) => {
-        if (err) {
-          return reject(new Error('unable to read file.'));
-        }
-        students = JSON.parse(studentData);
-        programs = JSON.parse(programData);
-        resolve();
-      });
+      students = JSON.parse(studentData);
     });
+    fs.readFile('./data/programs.json', 'utf8', (err, programData) => {
+      if (err) {
+        reject(new Error('unable to read file.')); return;
+      }
+      programs = JSON.parse(programData);
+    });
+    resolve();
   });
 };
 
+// Students
 module.exports.getAllStudents = function() {
   return new Promise((resolve, reject) => {
     if (students.length == 0) {
-      return reject(new Error('no results returned.'));
+      reject(new Error('no results returned.')); return;
     }
     resolve(students);
   });
 };
+module.exports.addStudent = function(studentData) {
+  return new Promise((resolve, reject) => {
+    let newStudent;
+    if (studentData.isInternationalStudent === undefined) {
+      newStudent = false;
+    } else {
+      newStudent = true;
+    }
 
-//
+    const createStudent = ({
+      firstName: studentData.firstName,
+      lastName: studentData.lastName,
+      email: studentData.email,
+      phone: studentData.phone,
+      addressStreet: studentData.addressStreet,
+      addressCity: studentData.addressCity,
+      addressState: studentData.addressState,
+      addressPostal: studentData.addressPostal,
+      expectedCredential: studentData.expectedCredential,
+      status: studentData.status,
+      program: studentData.program,
+      registrationDate: studentData.registrationDate,
+      isInternationalStudent: newStudent,
+    });
+    students.push(createStudent);
+    if (createStudent.length == 0) {
+      reject(new Error('no results returned. (By: addStudent')); return;
+    }
+    resolve(createStudent);
+  });
+};
+module.exports.getStudentsByStatus = function(num) {
+  const fullTime = [];
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < students.length; i++) {
+      if (students[i].status == num) {
+        fullTime.push(students[i]);
+      }
+    }
+    if (fullTime.length == 0) {
+      reject(new Error('No results found (By: Status)')); return;
+    }
+    resolve(fullTime);
+  });
+};
+module.exports.getStudentsByProgramCode = function(num) {
+  const program = [];
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < students.length; i++) {
+      if (students[i].program == num) {
+        program.push(students[i]);
+      }
+    }
+    if (program.length == 0) {
+      reject(new Error('No results found (By: Status)')); return;
+    }
+    resolve(program);
+  });
+};
+module.exports.getStudentsByCredentials = function(num) {
+  const creds = [];
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < students.length; i++) {
+      if (students[i].expectedCredential == num) {
+        creds.push(students[i]);
+      }
+    }
+    if (creds.length == 0) {
+      reject(new Error('No results found (By: Status)')); return;
+    }
+    resolve(creds);
+  });
+};
+module.exports.getStudentsByID = function(num) {
+  const sid = [];
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < students.length; i++) {
+      if (students[i].studentID == num) {
+        sid.push(students[i]);
+      }
+    }
+    if (sid.length == 0) {
+      reject(new Error('No results found (By: Status)')); return;
+    }
+    resolve(sid);
+  });
+};
 module.exports.getInternationalStudents = function() {
   return new Promise((resolve, reject) => {
     const intlstudentsData = [];
@@ -39,17 +125,20 @@ module.exports.getInternationalStudents = function() {
       }
     }
     if (intlstudentsData.length == 0) {
-      return reject(new Error('no results returned.'));
+      reject(new Error('no results returned.')); return;
     }
     resolve(intlstudentsData);
   });
 };
 
+// Programs
 module.exports.getPrograms = function() {
   return new Promise((resolve, reject) => {
     if (programs.length == 0) {
-      return reject(new Error('no results returned'));
+      reject(new Error('no results returned')); return;
     }
     resolve(programs);
   });
 };
+
+
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 /** *******************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including 3rd party web sites) or distributed to other students.
 *
-*  Name: Christian Park   Student ID: 036917128  Date: Sept. 29, 2022
+*  Name: Christian Park   Student ID: 036917128  Date: Oct. 10, 2022
 *
-*  Online (Cyclic) Link:
+*  Online (Cyclic) Link: https://alive-overshirt-frog.cyclic.app/about
 *
 ********************************************************************************/
 
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+
+const storage = multer.diskStorage({
+  destination: './public/images/uploaded',
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({storage: storage});
+
 const app = express();
 const data = require('./data-service.js');
+const path = require('path');
+
 
 const HTTP_PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
 
+// Initial
+app.use(express.urlencoded({extended: true}));
 data.initialize().then(function() {
   app.listen(HTTP_PORT, function() {
     console.log('Express http server listening on port ' + HTTP_PORT);
@@ -25,34 +40,97 @@ data.initialize().then(function() {
   console.log('Unable to start server: ' + err);
 });
 
-// setup a 'route' to listen on the default url path (http://localhost)
+// Home
 app.get('/', function(req, res) {
   res.sendFile('./views/home.html', {root: __dirname});
 });
 
-// setup another route to listen on /about
+// About
 app.get('/about', function(req, res) {
   res.sendFile('./views/about.html', {root: __dirname});
 });
 
+// Students
 app.get('/students', function(req, res) {
-  data.getAllStudents().then((data) => {
-    res.json(data);
+  if (req.query.status) {
+    data.getStudentsByStatus(req.query.status).then((data)=>{
+      if (data.length > 0) {
+        res.json(data);
+      } else {
+        res.send('No results :Status');
+      }
+    });
+  } else if (req.query.program) {
+    data.getStudentsByProgramCode(req.query.program).then((data)=>{
+      if (data.length > 0) {
+        res.json(data);
+      } else {
+        res.send('No results :Program');
+      }
+    });
+  } else if (req.query.credential) {
+    data.getStudentsByExpectedCredential(req.query.credential).then((data)=>{
+      if (data.length > 0) {
+        res.json(data);
+      } else {
+        res.send('No results :Credentials');
+      }
+    });
+  } else {
+    data.getAllStudents().then((data) => {
+      res.json(data);
+    });
+  }
+});
+
+// Add Student
+app.get('/students/add', function(req, res) {
+  res.sendFile('./views/addStudent.html', {root: __dirname});
+});
+app.post('/students/add', function(req, res) {
+  data.addStudent(req.body).then(()=>{
+    res.redirect('/students');
   });
 });
 
+app.get('/students/:studentID', function(req, res) {
+  data.getStudentsByID(req.params.studentID).then((data)=>{
+    res.json(data);
+  }).catch(function(err) {
+    res.send('No studentID found');
+  });
+});
+
+// Add Image
+app.get('/images/add', function(req, res) {
+  res.sendFile('./views/addImage.html', {root: __dirname});
+});
+app.post('/images/add', upload.single('imageFile'), (req, res) => {
+  res.redirect('/images');
+});
+
+// Images
+app.get('/images', function(req, res) {
+  fs.readdir('./public/images/uploaded', function(err, items) {
+    res.json('images :' + items);
+  });
+});
+
+// International Students
 app.get('/intlstudents', function(req, res) {
   data.getInternationalStudents().then((data) => {
     res.json(data);
   });
 });
 
+// Programs
 app.get('/programs', function(req, res) {
   data.getPrograms().then((data) => {
     res.json(data);
   });
 });
 
+// Error
 app.get('*', function(req, res) {
   res.send('Error Code: 404', 404);
 });
